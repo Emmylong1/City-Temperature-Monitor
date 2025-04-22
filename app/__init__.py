724bcd76fd290e2bb849b1b600f4d3dc7c91c798
Ibok Emmanuel <98855013+Emmylong1@users.noreply.github.com>
2025-04-22 09:20:28 +0100

    Delete app directory

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/__init__.py
-"""
-App package initializer.
-"""
-
-# You can import key modules here if needed
-# from .processor import process_data
--- app/processor.py
+++
@@ -1,6 +0,0 @@
-def process_temperature_data(city, temperature):
-    """
-    Process the temperature data.
-    Here we simply format the data.
-    """
-    return f"City: {city}, Temperature: {temperature}"
